6e5124eece9dcb563b2a7065abf666da77c069de
mumu111111 <2646709035@qq.com>
2019-09-19 22:31:00 +0800

    'router配置'

---
+++ src/router/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router = new Router({
+    routes: [
+        {
+            path: '/',
+            component:　()=>import('@/components/home.vue')
+        },
+        {
+            path: '/about',
+            component:　()=>import('@/components/about.vue')
+            
+
+        },
+        {
+            path: '/topic/:topicId',
+            component:　()=>import('@/components/topic.vue')
+
+        }
+    ]
+})
+export default router
